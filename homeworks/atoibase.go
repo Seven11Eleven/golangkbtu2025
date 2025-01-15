ff286017a99080fa35c01876d0ca4819288b1758
Seven11Eleven <toleuhanernur0@gmail.com>
2025-01-15 22:12:36 +0500

    add homework practice1

--- homeworks/atoibase.go
+++ homeworks/atoibase.go
@@ -1,5 +1,33 @@
 package homeworks
 
 func AtoiBase(s string, base string) int {
+	if !isValidBase(base) {
+		return 0
+	}
 
+	baseMapa := make(map[rune]int)
+	for i, char := range base {
+		baseMapa[char] = i
+	}
+
+	res := 0
+	for _, char := range s {
+		res = res*len(base) + baseMapa[char]
+	}
+	return res
+}
+
+func isValidBase(base string) bool {
+	if len(base) < 2 {
+		return false
+	}
+
+	charSet := make(map[rune]bool)
+	for _, char := range base {
+		if char == '+' || char == '-' || charSet[char] {
+			return false
+		}
+		charSet[char] = true
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -2,47 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/Seven11Eleven/golangkbtu2025/practices"
+	"github.com/Seven11Eleven/golangkbtu2025/homeworks"
 )
 
-func runTests() {
-	tests := []struct {
-		input    string
-		expected string
-		error    bool
-	}{
-		{"a4bc2d5e", "aaaabccddddde", false},
-		{"abcd", "abcd", false},
-		{"3abc", "", true},
-		{"45", "", true},
-		{"aaa10b", "", true},
-		{"aaa0b", "aab", false},
-		{"", "", false},
-		{"d\n5abc", "d\n\n\n\n\nabc", false},
-		{"qwe\\4\\5", "qwe45", false},
-		{"qwe\\45", "qwe44444", false},
-		{"qwe\\\\\\5", "qwe\\\\\\", false},
-		{"qw\ne", "", true},
-	}
-
-	for _, test := range tests {
-		result, err := practices.Unpack(test.input)
-		if test.error {
-			if err == nil {
-				fmt.Printf("FAIL: Input: %q -> Expected error but got result: %q\n", test.input, result)
-			} else {
-				fmt.Printf("PASS: Input: %q -> Expected error and got error: %v\n", test.input, err)
-			}
-		} else {
-			if err != nil || result != test.expected {
-				fmt.Printf("FAIL: Input: %q -> Expected: %q, Got: %q, Error: %v\n", test.input, test.expected, result, err)
-			} else {
-				fmt.Printf("PASS: Input: %q -> Output: %q\n", test.input, result)
-			}
-		}
-	}
-}
-
 func main() {
-	runTests()
+	fmt.Println(homeworks.AtoiBase("125", "0123456789"))
+	fmt.Println(homeworks.AtoiBase("1111101", "01"))
+	fmt.Println(homeworks.AtoiBase("7D", "0123456789ABCDEF"))
+	fmt.Println(homeworks.AtoiBase("uoi", "choumi"))
+	fmt.Println(homeworks.AtoiBase("bbbbbab", "-ab"))
 }
+
+/*
+$ go run .
+125
+125
+125
+125
+0
+$
+*/
